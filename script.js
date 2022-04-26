ae87b5a297dd41817c5927352f7780482193738f
Rafael Alvarado <>
2022-04-26 21:45:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const totalPriceElement = document.getElementById("totalPrice");
+const parentListElement = document.getElementById("list");
+let totalPrice = 0;
+let cart = [];
+
+class Item{
+  constructor(name, price){
+    this.name = name;
+    this.price = price;
+  }
+}
+
+function refreshUI(){
+  //Draw the UI
+  totalPriceElement.innerText = `Total price: $${totalPrice} | Total # of items ${cart.length}`
+  parentListElement.innerHTML = "";
+  console.log(cart);
+  cart.forEach((item, index) =>{
+    const listElement = document.createElement("li");
+    const textNode = document.createTextNode(`${item.name} - $${item.price}`);
+    listElement.appendChild(textNode);
+    parentListElement.appendChild(listElement);
+    listElement.classList.add("list-group-item", "d-flex", "justify-content-between");
+
+    const deleteButton = document.createElement("button");
+    const deleteTextNode = document.createTextNode("delete");
+    deleteButton.appendChild(deleteTextNode);
+    deleteButton.classList.add("btn", "btn-danger");
+    listElement.appendChild(deleteButton);
+
+    deleteButton.addEventListener("click", () => {
+      cart.splice(index, 1);
+      totalPrice -= item.price;
+      refreshUI();
+    });
+  });
+}
+
+function addItem(form) {
+  const itemName = form.itemName.value;
+  const itemPrice = form.itemPrice.value;
+
+  totalPrice += parseInt(itemPrice);
+  const item = new Item(itemName, parseInt(itemPrice));
+  cart.push(item);
+
+  refreshUI();
+
+  return false;
+}
